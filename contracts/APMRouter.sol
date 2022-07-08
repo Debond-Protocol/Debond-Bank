cfd4a2a4ee414b04f8872d628daad09c1e2b1ece
Toufic Batrice <touficbatrice@gmail.com>
2022-07-08 17:00:13 +0200

    swap eth

--- contracts/APMRouter.sol
+++ contracts/APMRouter.sol
@@ -30,7 +30,37 @@ abstract contract APMRouter {
         address to
     ) external {
         uint[] memory amounts = apm.getAmountsOut(amountIn, path);
-        require(amounts[amounts.length - 1] >= amountOutMin, 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(amounts[amounts.length - 1] >= amountOutMin, 'APMRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+
+        IERC20(path[0]).transferFrom(msg.sender, address(apm), amounts[0]);
+        _swap(amounts, path, to);
+    }
+    function swapExactTokensForEth(
+        uint amountIn,
+        uint amountEthMin,
+        address[] calldata path,
+        address to
+    ) external {
+        require(path[path.length - 1] == Weth, 'APMRouter: INVALID_PATH');
+        uint[] memory amounts = apm.getAmountsOut(amountIn, path);
+        require(amounts[amounts.length - 1] >= amountEthMin, 'APMRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+
+        IERC20(path[0]).transferFrom(msg.sender, address(apm), amounts[0]);
+        _swap(amounts, path, address(this));
+        Iweth(Weth).withdraw(amounts[amounts.length - 1]);
+        payable(to).transfer(amounts[amounts.length - 1]);
+    }
+    function swapExactEthForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to
+    ) external {
+        require(path[0] == WETH, 'APMRouter: INVALID_PATH');
+        uint[] memory amounts = apm.getAmountsOut(amountIn, path);
+        require(amounts[amounts.length - 1] >= amountOutMin, 'APMRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        IWeth(WETHAddress).deposit{value : amountIn}();
+        assert(IWeth(WETHAddress).transfer(address(apm), amountIn));
 
         IERC20(path[0]).transferFrom(msg.sender, address(apm), amounts[0]);
         _swap(amounts, path, to);
