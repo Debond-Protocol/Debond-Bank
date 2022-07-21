e5c31433d9021a2b735592343cdf455b29de2c88
toufic0710 <67787511+toufic0710@users.noreply.github.com>
2022-07-21 18:04:35 +0200

    redeem tests ok (#20)

--- migrations/7_deploy_bankData.js
+++ migrations/7_deploy_bankData.js
@@ -8,6 +8,7 @@ module.exports = async function (deployer, networks, accounts) {
 
   const bankBondManaerInstance = await BankBondManager.deployed();
   let d = new Date();
+  d.setDate(d.getDate() - 1);
   d.setHours(0,0,0,0);
   await deployer.deploy(BankData, governanceAddress, bankBondManaerInstance.address, d.getTime()/10**3);
 
