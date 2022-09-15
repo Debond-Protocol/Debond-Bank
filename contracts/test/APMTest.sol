b92df3416754c3d3953b9ee968f32723430d83cd
Toufic <touficbatrice@gmail.com>
2022-09-15 11:27:31 +0200

    compiling

--- contracts/test/APMTest.sol
+++ contracts/test/APMTest.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 
 
 import "@debond-protocol/debond-apm-contracts/APM.sol";
-import "@openzeppelin/contracts/token/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract APMTest is APM {
 
     constructor(address governanceAddress, address bankAddress) APM(governanceAddress, bankAddress) {}
 
-    function removeLiquidityInsidePool(address _to, address _tokenA, address DBITAddress, uint _amountDbitToBurn) public {
-        IERC20(DBITAddress).mint();
-    }
+    /*function removeLiquidityInsidePool(address _to, address _tokenA, address DBITAddress, uint _amountDbitToBurn) public {
+        //IERC20(DBITAddress).mint();
+    }*/
 } 
